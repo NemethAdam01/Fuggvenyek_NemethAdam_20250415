6d6083a2034d8ecdd7bd9fa14154af380c5a111c
NemethAdam01 <nemeth.adam@diak.szbi-pg.hu>
2025-04-15 10:38:23 +0200

    create

---
+++ fuggvenyek.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a + b;
+    }
+    console.log(add(3, 2));
+
+let add2 = function(a, b) {
+    return a + b;
+    }
+    console.log(add(3, 2));
+
+let add3 = (a, b) => a + b;
+console.log(add3(5, 3));
+
+(function() {
+    let nev = prompt("Adja meg a nevét!"),
+    udv = function() {
+    return "Szia! " + nev + " vagyok!";
+    }
+    let vmi = document.getElementById("szoveg");
+    vmi.innerText = "Szia! " + nev + " vagyok!";
+    console.log(udv());
+    }());
